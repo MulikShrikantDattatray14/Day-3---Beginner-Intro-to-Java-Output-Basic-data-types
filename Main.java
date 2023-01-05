23c311aaa010ebfb1e9e6c90db5ed31ee51d2087
Mulik Shrikant Dattatray <>
2023-01-05 02:17:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+ 
+//   Q1. Hello World ! - 4
+
+
+// Print the following text:
+
+// Hello 40
+// 50 World !
+
+// Output Format
+// Print the output specifed in the problem description.
+
+
+// Example Output
+// Hello 40
+// 50 World !
+
+
+
